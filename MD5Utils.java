fc4c5fb6499545523d5ca8101a5d80fa0d8d591c
carrys17 <961513094@qq.com>
2017-12-29 21:10:58 +0800

    MD5utils（对字符串采用MD5加密）

---
+++ MD5Utils.java
@@ -0,0 +1,32 @@
+
+import java.security.MessageDigest;
+
+public class MD5Util {
+	// 对字符串采用MD5加密
+	public static String MD5Encode(String inStr) throws Exception {
+
+        MessageDigest md5 = null;
+
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+            return "";
+
+        }
+
+
+        byte[] byteArray = inStr.getBytes("UTF-8");
+        byte[] md5Bytes = md5.digest(byteArray);
+        StringBuffer hexValue = new StringBuffer();
+        for (int i = 0; i < md5Bytes.length; i++) {
+            int val = ((int) md5Bytes[i]) & 0xff;
+            if (val < 16) {
+                hexValue.append("0");
+            }
+            hexValue.append(Integer.toHexString(val));
+        }
+        return hexValue.toString();
+    }
+}
