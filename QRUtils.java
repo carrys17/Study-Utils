0e0abca99ad34144d97989bb19f61f35d2e92a71
carrys17 <961513094@qq.com>
2018-02-09 23:03:38 +0800

    新增利用zxing将url转换为二维码图片

---
+++ QRUtils.java
@@ -0,0 +1,76 @@
+﻿package com.example.shang.testget;
+
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by admin on 2018/2/8.
+ */
+
+public class QRUtils {
+
+    /**
+     *  生成二维码到指定的路径
+     * @param content    要生成二维码的url
+     * @param widthPix   宽
+     * @param heightPix  高
+     * @param filePath   存放的路径
+     * @return  是否创建成功
+     * @throws WriterException
+     * @throws FileNotFoundException
+     */
+    public static boolean createQRImage(String content, int widthPix, int heightPix, String filePath) throws WriterException, IOException {
+
+        if (content == null || "".equals(content)) {
+            return false;
+        }
+
+        Log.i("http", "filePath -- "+filePath);
+
+        //配置参数
+        Map<EncodeHintType, Object> hints = new HashMap<>();
+        hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+        //容错级别
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+        //设置空白边距的宽度
+        hints.put(EncodeHintType.MARGIN, 2); //default is 4
+
+        // 图像数据转换，使用了矩阵转换
+        BitMatrix bitMatrix = new QRCodeWriter().encode(content, BarcodeFormat.QR_CODE, widthPix, heightPix, hints);
+        int[] pixels = new int[widthPix * heightPix];
+        // 下面这里按照二维码的算法，逐个生成二维码的图片，
+        // 两个for循环是图片横列扫描的结果
+        for (int y = 0; y < heightPix; y++) {
+            for (int x = 0; x < widthPix; x++) {
+                if (bitMatrix.get(x, y)) {
+                    pixels[y * widthPix + x] = 0xff000000;
+                } else {
+                    pixels[y * widthPix + x] = 0xffffffff;
+                }
+            }
+        }
+
+        // 生成二维码图片的格式，使用ARGB_8888
+        Bitmap bitmap = Bitmap.createBitmap(widthPix, heightPix, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, widthPix, 0, 0, widthPix, heightPix);
+
+
+        //必须使用compress方法将bitmap保存到文件中再进行读取。直接返回的bitmap是没有任何压缩的，内存消耗巨大！
+        return bitmap != null && bitmap.compress(Bitmap.CompressFormat.JPEG, 100, new FileOutputStream(filePath));
+
+    }
+}
