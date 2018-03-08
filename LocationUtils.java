96edf74c6003940a77ee18f0a06634de0dbbea54
carrys17 <961513094@qq.com>
2018-03-08 20:28:56 +0800

    新增由给定两个点的经纬度，计算出这两点的距离

--- LocationUtils.java
+++ LocationUtils.java
@@ -1,4 +1,4 @@
-package com.example.admin.locationdemo;
+﻿package com.example.admin.locationdemo;
 
 import android.util.Log;
 
@@ -126,4 +126,29 @@ public class LocationUtils {
             return "读取失败 e -- "+e.getMessage();
         }
     }
+
+
+ 	/**
+         * 计算两个坐标点之间的距离
+         *
+         * @param firstLatitude   第一个坐标的纬度
+         * @param firstLongitude  第一个坐标的经度
+         * @param secondLatitude  第二个坐标的纬度
+         * @param secondLongitude 第二个坐标的经度
+         * @return 返回两点之间的距离，单位：m
+         */
+        public static double getDistance(double firstLatitude, double firstLongitude,
+                                         double secondLatitude, double secondLongitude) {
+            double firstRadLat = rad(firstLatitude);
+            double firstRadLng = rad(firstLongitude);
+            double secondRadLat = rad(secondLatitude);
+            double secondRadLng = rad(secondLongitude);
+
+            double a = firstRadLat - secondRadLat;
+            double b = firstRadLng - secondRadLng;
+            double cal = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(firstRadLat)
+                    * Math.cos(secondRadLat) * Math.pow(Math.sin(b / 2), 2))) * EarthRadius;
+            double result = Math.round(cal * 10000d) * 1000d / 10000d ;
+            return result;
+        }
 }
